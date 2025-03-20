8d33fe2a05f319340d9810e356fb03cca41b20ee
Olaf Loewe <79728894+olafloewe@users.noreply.github.com>
2025-03-20 20:07:55 +0100

    Add files via upload

---
+++ DrawingBoardAppStep0/DrawingBoardApp/DrawingBoard.cs
@@ -0,0 +1,32 @@
+﻿using System.Drawing;
+using System;
+
+namespace DrawingBoardApp
+{
+    class DrawingBoard
+    {
+        private int tick;
+		private int scrWidth, scrHeight;
+
+        public DrawingBoard(MainForm mainForm)
+        {
+            scrWidth = mainForm.ClientSize.Width;
+            scrHeight = mainForm.ClientSize.Height;
+			tick = 0;
+        }
+
+        public void Draw(Graphics g)
+        {
+            // Clear the screen - for this frame
+            g.Clear(Color.FromArgb(228, 228, 228));
+
+            g.DrawEllipse(Pens.Red, 400, 200, 20, 20);
+        }
+
+        public void Tick()
+        {
+            tick += 1;
+            Console.WriteLine("Tick {0}", tick);
+        }
+    }
+}
